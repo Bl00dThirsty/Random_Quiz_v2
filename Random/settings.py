88ffc388acb9b75f040907aa92a448bfe28341b5
yvana2005 <ivanaazambou@gmail.com>
2023-09-19 17:43:39 +0100

    quiz_app

--- Random/settings.py
+++ Random/settings.py
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'apps.accounts',
     'apps.home',
+    'apps.quiz',
     'django_extensions',
 ]
 
--- Random/urls.py
+++ Random/urls.py
@@ -5,4 +5,8 @@ from django.urls import path, include
 urlpatterns = [
     path('', include('apps.home.urls')),
     path('admin/', admin.site.urls),
-]
+    path('', index, name='index'),
+    path('admin/', admin.site.urls),
+    path('signup/', signup, name="signup"),
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+
