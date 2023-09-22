19430632a5bbdaa7c94a23d02c340a4b10d8ac1f
yvana2005 <ivanaazambou@gmail.com>
2023-09-22 11:22:54 +0100

    sauvegarde5

--- apps/accounts/views.py
+++ apps/accounts/views.py
@@ -9,6 +9,7 @@ def signup(request, *args, **kwargs):
     if request.method == 'POST':
         name = request.POST['name']
         forename = request.POST['forename']
+        code = request.POST['code']
         date = request.POST['date']
         email = request.POST['email']
         phone_number = request.POST['phone_number']
@@ -17,6 +18,7 @@ def signup(request, *args, **kwargs):
     #.signup != .create
         saved_name = request.session.get('name')
         saved_forename = request.session.get('forename')
+        saved_code = request.session.get('code')
         saved_date = request.session.get('date')
         saved_email = request.session.get('email')
         saved_phone = request.session.get('phone_number')
@@ -28,7 +30,7 @@ def signup(request, *args, **kwargs):
             return redirect('error')
 
         else:
-            newCandidat = Candidat(name=name, forename=forename, date=date, email=email, phone_number=phone_number, password=password)
+            newCandidat = Candidat(name=name, forename=forename, code=code,  date=date, email=email, phone_number=phone_number, password=password)
             newCandidat.save()
             return redirect('home')
     
