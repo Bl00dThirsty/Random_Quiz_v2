38aef0e9e98b7e1f6c6bf9103c55a98b9591a827
John Davis <johndavisambassa@gmail.com>
2023-10-02 21:37:31 +0100

    score-box

--- apps/static/assets/js/quiz.js
+++ apps/static/assets/js/quiz.js
@@ -1,69 +1,102 @@
-const progressBar = document.querySelector(".progress-bar"),
-  progressText = document.querySelector(".progress-text");
+"use strict";
+const quizBox = document.querySelector('.quiz-box');
+    const nextBtn = document.querySelector('.next-btn');
+    var app = new Vue({
+      el: '#app',
+      delimiters: ['[[', ']]'],
+      data() {
+        return {
+          category: '{{category}}',
+          questions: [],
+          currentQuestion: null,
+          userScore: 0,
+        };
+      },
+      methods: {
+        getQuestions() {
+            var _this = this;
+            return fetch(`/api/get-quiz/?category=${this.category}`)
+                .then(response => response.json())
+                .then(result => {
+                console.log(result);
+                _this.questions = result.data;
+                if (_this.questions.length > 0) {
+                    _this.currentQuestion = _this.questions[0];
+                }
+                });
+            },
+        
+        checkAnswer(event, uid, selectedAnswer) {
+          this.questions.forEach(question => {
+          if (question.uid === uid && !question.answered) {
+            question.answered = true;
+              if (selectedAnswer) {
+                if (selectedAnswer.is_correct) {
+                  console.log('reponse juste');
+                  alert('hurrrr correcte');
+                  userScore += 1;
+                } else {
+                  console.log('reponse fausse');
+                }
+              }
+          }
+          if (question.uid !== uid) {
+            question.disabled = true;
+            console.log('desactiver');
+          }
+          });
+        },
+        showResultBox() {
+          quizBox.classList.remove('active');
+          result_box.classList.add('active');
 
-const progress = (value) => {
-  const percentage = (value / time) * 100;
-  progressBar.style.width = `${percentage}%`;
-  progressText.innerHTML = `${value}`;
-};
+          const score_text = document.querySelector('.score-text');
+          score_text.textContent = `Votre score est de ${userScore} sur ${questions.length}`;
 
-const startBtn = document.querySelector(".start"),
-  quiz = document.querySelector(".quiz"),
-  startScreen = document.querySelector(".start-screen");
-
-let questions = [],
-  time = 30,
-  score = 0,
-  currentQuestion,
-  timer;
+          if(userScore < 5){
+              TryAgain.classList.add('active');
+          }
+          else{
+              TryAgain.classList.remove('active');
+          }
 
 
-const startTimer = (time) => {
-  timer = setInterval(() => {
-    if (time >= 0) {
-      progress(time);
-      time--;
-    } else {
-      checkAnswer();
-    }
-  }, 1000);
-};
 
-const loadingAnimation = () => {
-  startBtn.innerHTML = "Loading";
-  const loadingInterval = setInterval(() => {
-    if (startBtn.innerHTML.length === 10) {
-      startBtn.innerHTML = "Loading";
-    } else {
-      startBtn.innerHTML += ".";
-    }
-  }, 500);
-};
+          const circularProgress = document.querySelector('.cicular-progress');
+          const progressValue = document.querySelector('.progress-value');
 
-const submitBtn = document.querySelector(".submit"),
-  nextBtn = document.querySelector(".next");
-submitBtn.addEventListener("click", () => {
-  checkAnswer();
-});
+          let progressStartValue = -1;
+          let progressEndValue = (userScore / questions.length) * 100;
+          let speed = 20;
 
-nextBtn.addEventListener("click", () => {
-  nextQuestion();
-  submitBtn.style.display = "block";
-  nextBtn.style.display = "none";
-});
+          let progress = setInterval(() => {
+              progressStartValue++;
+              //console.log(progressStartValue);
+              progressValue.textContent = `${progressStartValue}%`;
+              circularProgress.style.background = `conic-gradient(#c40094 ${progressStartValue * 3.6}deg, rgba(255, 255, 255, .1) 0deg)`;
 
 
-const endScreen = document.querySelector(".end-screen"),
-  finalScore = document.querySelector(".final-score"),
-  totalScore = document.querySelector(".total-score");
-const showScore = () => {
-  endScreen.classList.remove("hide");
-  quiz.classList.add("hide");
-  finalScore.innerHTML = score;
-  totalScore.innerHTML = `/ ${questions.length}`;
-};
+              if (progressStartValue == progressEndValue) {
 
-const restartBtn = document.querySelector(".restart");
-restartBtn.addEventListener("click", () => {
-  window.location.reload();
-});
+                  clearInterval(progress);
+
+              }
+
+          }, speed);
+        },
+        nextQuestion() {
+            const currentIndex = this.questions.indexOf(this.currentQuestion);
+            const nextIndex = currentIndex + 1;
+            if (nextIndex < this.questions.length) {
+            this.currentQuestion = this.questions[nextIndex];
+            }
+            else {
+            this.showResultBox();
+            }
+        },
+        
+      },
+      created() {
+        this.getQuestions();
+      }
+    });
